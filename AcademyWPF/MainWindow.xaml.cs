a1206129be381f1c44856c7347d3afaeb9572e69
Булат <4bxlia4@gmail.com>
2026-06-09 18:51:36 +0500

    AcademyWPF

---
+++ AcademyWPF/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Configuration;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+using DBtools;
+
+namespace AcademyWPF
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		Connector connector;
+		DataGrid[] tables;
+		public MainWindow()
+		{
+			InitializeComponent();
+			connector = new Connector(ConfigurationManager.ConnectionStrings["P_421_Import"].ConnectionString);
+			tables = new DataGrid[] { dgStudents, dgGroups, dgDirections, dgDisiplines, dgTeachers };
+		}
+
+		private void tabControl_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+		{
+			int i = (sender as TabControl).SelectedIndex;
+			tables[i].ItemsSource = connector.Load($"SELECT * FROM {(tabControl.Items[i] as TabItem).Header}").DefaultView;
+		}
+	}
+}
